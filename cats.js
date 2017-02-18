5ebc5a91c85f3de9fe3cd1710f5fd523979bd859
wtung008 <wayne.h.tung@gmail.com>
2017-02-17 22:27:49 -0500

    Making the Ajax request

--- cats.js
+++ cats.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
   $(".summon-cats").on('click', function(){
-    console.log("it works!!")
+    // console.log("it works!!")
+    $.ajax({
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      method: 'GET',
+      data: {},
+      dataType: 'json'
+    }).done(function(responseData){
+      console.log(responseData);
+    });
   });
 
 });
