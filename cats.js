bee7184959858a89df272441eb17de4e46ca55ec
wtung008 <wayne.h.tung@gmail.com>
2017-02-18 02:19:14 -0500

    Adding cats to the DOM

--- cats.js
+++ cats.js
@@ -8,6 +8,15 @@ $(document).ready(function() {
       dataType: 'json'
     }).done(function(responseData){
       console.log(responseData);
+
+      console.log(responseData.cats[0]["name"])
+      // $.each(responseData.cats, function(key, value) {
+      //   console.log(value.name + "  source: " + value.photo);
+
+        $('<img>').attr('src', responseData.cats[0]["photo"], 'alt', "Photo of " + responseData.cats[0]["name"]).appendTo('#cat1');
+        $('<img>').attr('src', responseData.cats[1]["photo"], 'alt', "Photo of " + responseData.cats[1]["name"]).appendTo('#cat2');
+        $('<img>').attr('src', responseData.cats[2]["photo"], 'alt', "Photo of " + responseData.cats[2]["name"]).appendTo('#cat3');
+    //   });
     });
   });
 
